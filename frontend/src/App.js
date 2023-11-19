69a02f5176fdd9c9885a470a3b2567a505e1876e
Morad17 <morad123@hotmail.co.uk>
2023-11-19 16:53:43 +0800

    created react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
+import {Outlet, createBrowserRouter, Routes, Route, RouterProvider} from 'react-router-dom'
+
 import './App.css';
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
+
+const Layout = () => {
+  return(
+   <div className="main-layout">
+    <Navbar />
+    <Outlet />
+  </div> 
+  )
+  
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [{
+      path:"/",
+      element: <Home />,
+
+    }
+  ]
+  }
+])
+
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
-  );N
+  );
 }
 
 export default App;
