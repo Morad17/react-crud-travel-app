0bdc29b1c4b731e5c04779adc07f6e762daae0f2
Morad17 <morad123@hotmail.co.uk>
2023-11-25 22:21:09 +0800

    updates

--- frontend/src/pages/AddTrip.jsx
+++ frontend/src/pages/AddTrip.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
 
 const AddTrip = () => {
+    const [trip, setTrip] = useState({
+        place:"",
+        date:"",
+        length:null,
+        activities:"",
+        location:""
+    })
+
+    const navigate = useNavigate()
+    const handleChange = (e) => {
+        setTrip(prev=>({...prev, [e.target.name]:e.target.value }))
+        console.log(trip);
+    }
+    const handleSubmit = async e => {
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8000/new-trip",trip)
+            navigate("/")
+            
+        } catch(err) {
+            console.log(err);
+        }
+    }
   return (
-    <div>AddTrip</div>
+    <div className="add-form">
+        <h1>Add A New Trip</h1>
+        <input type="text" placeholder="Place Name" onChange={handleChange} name="place"/>
+        <input type="date" placeholder="Date Visited (year/month/day)" onChange={handleChange} name="date"/>
+        <input type="number" placeholder="Length Of Stay (days)" onChange={handleChange} name="length"/>
+        <input type="text" placeholder="Activities" onChange={handleChange} name="activities"/>
+        <input type="text" placeholder="Google Maps Link" onChange={handleChange} name="location"/>
+        <button className="add-trip-button" onClick={handleSubmit}>Submit</button>
+    </div>
   )
 }
 
