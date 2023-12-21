d1ea8debb0d3f35101a9f71d87089e74a3d559b3
Morad17 <morad123@hotmail.co.uk>
2023-12-21 23:40:17 +0800

    updare

---
+++ frontend/src/pages/Country.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Country = ({}) => {
+  return (
+    <div className="country-page">
+        <h1>Country</h1>
+        
+    </div>
+  )
+}
+
+export default Country
