401406eab312fcabc1027903248bffc8a6188411
Morad17 <morad123@hotmail.co.uk>
2024-01-18 18:22:47 +0000

    finishing upload form

--- frontend/src/pages/admin/AdminHome.jsx
+++ frontend/src/pages/admin/AdminHome.jsx
@@ -3,22 +3,24 @@ import axios from 'axios'
 import { useNavigate } from 'react-router'
 
 import ReactFlagsSelect from 'react-flags-select'
+import { getName } from 'country-list'
 
 const AdminHome = () => {
 
     const [file, setFile] = useState()
     const [tags, setTags] = useState()
-    const [selectedCountry, setSelectedCountry] = useState("")
+    const [selected, setSelected] = useState("")
     const [ tripData, setTripData] = useState({
         date:null,
         place_name:"",
         province:null,
         city:"",
         country:"",
-        tags: null
+        tags: ""
 
     })
-      
+
+    // Get All Tags
     useEffect(()=>{
         const allTags = async () => {
             try{
@@ -36,18 +38,33 @@ const AdminHome = () => {
     const navigate= useNavigate()
     const handleChange = (e) => {
         setTripData(prev=>({...prev, [e.target.name]:e.target.value}))
+        console.log(tripData);
     }
+    //Adding Country to form //
+    const handleCountry = () => {
+        const country = getName(selected)
+        setTripData( prev => ({
+            ...prev, country:country
+        }))
 
-    const submit = async e => {
-        e.preventDefault()
+    }
+    // Adding All Selected Tags to Form //
+    const handleTags = () => {
         const tagElements = document.getElementsByClassName("tag-checkbox")
         const tagElementsArray = Array.from(tagElements)
         const selectedTags = tagElementsArray.filter(tag => tag.checked === true)
             .map(tag => tag.name)
+            .toString()
         setTripData(prev => ({
             ...prev, tags: selectedTags
-        }))
-
+        }))}
+       
+    const submit = async e => {
+        
+        e.preventDefault()
+        handleCountry()
+        handleTags()
+        //Appending file with All form data //
         const formData = new FormData()
         formData.append("images", file)
         formData.append("photo_name",tripData.photo_name)
@@ -69,42 +86,47 @@ const AdminHome = () => {
     }
 
   return (
-    <div className="">
+    <div className="admin-home">
+        
         <form onSubmit={submit} encType="multipart/form-data">
+            <h2>Upload Your Photo</h2>
             <div className="field">
-                <label htmlFor=""></label>
-                <input onChange={fileSelected} name="images" accept="image/*" type="file" />
+                <label >Choose A Photo</label>
+                <input required onChange={fileSelected} name="images" accept="image/*" type="file" />
             </div>
             <div className="field">
                 <label htmlFor="">Date</label>
-                <input name="date"type="date" placeholder="Date Visited (year/month/day)" onChange={handleChange} />
+                <input required name="date"type="date" placeholder="Date Visited (year/month/day)" onChange={handleChange} />
             </div>
             <div className="field">
                 <label htmlFor="">Place Name</label>
-                <input name="place_name" onChange={handleChange} type="text" />
+                <input required name="place_name" onChange={handleChange} type="text" />
             </div>
             <div className="field">
                 <label htmlFor="">Country</label>
-                <ReactFlagsSelect selected={selectedCountry} onSelect={(code) => setSelectedCountry(code)} />
+                <ReactFlagsSelect selected={selected} onSelect={(code) => setSelected(code)} />
             </div>
             <div className="field">
                 <label htmlFor="">Province</label>
-                <input name="province" onChange={handleChange} type="text" />
+                <input required name="province" onChange={handleChange} type="text" />
             </div>
             <div className="field">
                 <label htmlFor="">City</label>
-                <input name="city" onChange={handleChange} type="text" />
+                <input required name="city" onChange={handleChange} type="text" />
             </div>
-            <div className="field">
+            <div className="field tag-checkbox-list">
                 <h3>Tags</h3>
-                {
+                <div className="tag-div">
+                    {
                     tags?.map((tag, key)=> {
-                        return <div className="tag-checkbox-list">
+                        return <div key={key} className="tag">
                             <label >{tag}</label>
-                            <input key={key} className="tag-checkbox" name={tag} type="checkbox" />
+                            <input   className="tag-checkbox" name={tag} type="checkbox" />
                         </div>
                     })
                 }
+                </div>
+                
             </div>
             <button type="submit">Submit</button>
         </form>
